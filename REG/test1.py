556f97a1bf44bb463d09abe1d4b663cd975abef7
Genji <1625238574@qq.com>
2025-09-29 13:28:52 +0100

    4

--- REG/test1.py
+++ REG/test1.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pylab as plt
+%matplotlib inline
 data=np.loadtxt('REG\olympic.txt',delimiter=',')
 print(data)
 print('ok')
+x=data[:,0]
+t=data[:,1]
+plt.scatter(x,t)
+plt.xlabel('Year')
+plt.ylabel('Time(seconds)')
+plt.show
